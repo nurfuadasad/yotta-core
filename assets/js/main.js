324dfc07daba863fada9590382c95f11d070eca6
unknown <sr_srabonbd@yahoo.com>
2022-07-26 16:32:07 +0600

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -403,7 +403,6 @@
         if ((el.children('div').length < 1) || (elSettings.items === '0' || elSettings.items === '' || typeof elSettings.items == 'undefined')) {
             return;
         }
-<<<<<<< HEAD
         var swiper = new Swiper('.yottaTestimonialOne', {
             slidesPerView: elSettings.items,
             spaceBetween: parseInt(elSettings.itemgap),
@@ -426,34 +425,6 @@
             breakpoints: {
                 991: {
                     slidesPerView: 3,
-=======
-
-        let $selector = '#' + el.attr('id');
-        let sliderSettings = {
-            infinite: elSettings.loop === 'yes',
-            slidesToShow: elSettings.items,
-            slidesToScroll: 1,
-            arrows: elSettings.nav === 'yes',
-            dots: elSettings.dot === 'yes',
-            autoplaySpeed: elSettings.autoplaytimeout,
-            autoplay: elSettings.autoplay === 'yes',
-            centerMode: elSettings.center === 'yes',
-            vertical: elSettings.vertical === 'yes',
-            appendArrows: $scope.find('.slick-carousel-controls .slider-nav'),
-            appendDots: $scope.find('.slick-carousel-controls .slider-dots'),
-            prevArrow: '<div class="prev-arrow">' + elSettings.navleft + '</div>',
-            nextArrow: '<div class="next-arrow">' + elSettings.navright + '</div>',
-            centerPadding: elSettings.centerpadding + 'px',
-            cssEase: 'linear',
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                        centerPadding: 0,
-                    }
->>>>>>> f9921c4e1c4b22ab5e44ad00f80a50440b03c3a0
                 },
                 767: {
                     slidesPerView: 2,
